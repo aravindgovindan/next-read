902887054db507619a279b5542ad4b34b058d343
Aravind <aravindgovindan001@gmail.com>
2023-01-14 01:03:40 -0700

    Add css

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import App from './App.vue'
 import VueRouter from 'vue-router'
 import HomePage from './components/HomePage.vue'
+import './main.css'
 
 Vue.config.productionTip = false
 
