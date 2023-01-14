d856c6ae99172e4dfca9c8828eca3fd5c2ea0d9e
Aravind <aravindgovindan001@gmail.com>
2023-01-13 23:43:54 -0700

    Initial commit

---
+++ src/components/HomePage.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="home">
+    <h1 class="title" align="center">Next Book</h1>
+    <div class="form-group" align="center">
+      <input class="form-control" v-model="bookTitle" placeholder="Enter a book title" />
+      <br>
+      <button class="btn btn-primary" @click="submitPrompt">Show next read</button>
+    </div>
+    <div v-if="recommendation">
+      <p>{{ recommendation }}</p>
+    </div>
+  </div>
+</template>
+
+
+<script>
+import axios from 'axios'
+
+export default {
+  data() {
+    return {
+      bookTitle: "",
+      recommendation: ""
+    }
+  },
+  methods: {
+    generatePrompt() {
+      return `You are a book suggester. Given a book the user likes, you need to suggest a new book that the user will like. You should answer with only a book title and author.
+Q. I liked reading Harry Potter and the goblet of fire. What should I read next?
+A. The Lightning Thief by Rick Riordan
+Q. I liked reading Twilight. What should I read next?
+A. Red Queen by Victoria Aveyard.
+Q. I liked reading ${this.bookTitle}. What should I read next?
+A. `
+    },
+    async submitPrompt() {
+      try {
+        const response = await axios.post('https://api.openai.com/v1/engines/babbage/completions', {
+          temperature: 0,
+          prompt: this.generatePrompt(),
+        }, {
+          headers: {
+            'Content-Type': 'application/json',
+            'Authorization': `Bearer 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`,
+          }
+        })
+        this.recommendation = response.data.choices[0].text.split('\n')[0]
+      } catch (error) {
+        console.log(error)
+      }
+    }
+  }
+}
+</script>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,23 @@
 import Vue from 'vue'
 import App from './App.vue'
+import VueRouter from 'vue-router'
+import HomePage from './components/HomePage.vue'
 
 Vue.config.productionTip = false
 
+
+Vue.use(VueRouter)
+
+const routes = [
+  { path: '/', component: HomePage },
+]
+
+const router = new VueRouter({
+  mode: 'history',
+  routes,
+});
+
 new Vue({
+  router,
   render: h => h(App),
 }).$mount('#app')
